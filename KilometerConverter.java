d915f46ea8e5078d5d64cf233d76e6ad9e245d21
Moksh Tehlan <tehlanmoksh29@gmail.com>
2020-09-06 10:58:29 +0530

    kilometers converter

---
+++ KilometerConverter.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+class KilometerConverter {
+
+    public static void main(String[] args){
+        Scanner km = new Scanner(System.in);
+        System.out.println("kilometer to miles converter:-)");
+        System.out.println("Enter the kilometers here:- ");
+        float kilo = km.nextFloat();
+        double miles = kilo*(0.621371);
+        System.out.println("Total Miles in "+kilo+" Kilometers are:- "+ miles);
+    }
+}
